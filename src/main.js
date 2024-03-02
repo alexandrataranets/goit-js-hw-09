d07d1bae98535bdc08d2ef6c73d73c44c6e1afea
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-03-02 21:11:36 +0100

    update src

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const linkListItems = document.querySelectorAll('.linkList');
+
+linkListItems.forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault(); 
+
+    const href = item.querySelector('a').getAttribute('href'); 
+
+    window.location.href = href; 
+  });
+});
+
+
+import {nanoid} from `nanoid`
+console.log(nanoid());
